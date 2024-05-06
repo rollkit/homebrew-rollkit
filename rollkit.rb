1560759198d139d7bcd3b88b7104b82077fd29b4
Matthew Sevey <15232757+MSevey@users.noreply.github.com>
2024-05-06 12:42:01 -0400

    chore: update the checksum for the tar file

--- rollkit.rb
+++ rollkit.rb
@@ -2,7 +2,7 @@ class Rollkit < Formula
 	desc "CLI tool for Rollkit"
 	homepage "https://rollkit.dev"
 	url "https://github.com/rollkit/rollkit/releases/download/v0.13.2/rollkit-cli-macos-v0.13.2.tar.gz"
-	sha256 "07e82abe71212523c230c141e0d60aedf4a999574555b448e470a3722bbd1ecf"
+	sha256 "4d43b1103c926268dfc1ac3b7c634538507c7fcc67f28166645089d58487f6b7"
       
 	# If needed, specify dependencies
 	# depends_on "dependency"
